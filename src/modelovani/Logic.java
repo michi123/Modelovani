41722fdcebaf23ec9f47104fec15107450717fed
Tomáš Jančar <jan02@modpreneur.com>
2015-11-24 13:53:00 +0100

    logic

--- src/modelovani/Logic.java
+++ src/modelovani/Logic.java
@@ -6,8 +6,32 @@ package modelovani;
  */
 public class Logic {
 
+    int numberOfDay = 1;
+    Year year;
+    
     public Logic() {
-        Osoba a = new Osoba();
+        year = new Year();
+        
+        for(int i = numberOfDay; i < 365; i++){
+           step();
+           numberOfDay++;
+        }   
+    }
+    
+    public void step(){    
+        Osoba actors[] = new Osoba[year.getNumberOfActorFromNumberOfDay(numberOfDay)];
+        for(int i = 0; i<actors.length; i++){
+            actors[i] = new Osoba();
+        }
+        
+        for(Osoba o: actors){
+            for(Osoba a: actors){
+                if( (a.getPoziceX() == o.getPoziceX()) && a.getPoziceY() == o.getPoziceY() ){
+                    System.out.println("A " + numberOfDay);
+                }
+            }
+        }
+        
     }
     
 }
